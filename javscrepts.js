285113f09b36b484c3bdd5385c5386f9da82ad22
العنود ناصر العجمي <70805765+alghanamanx@users.noreply.github.com>
2020-10-22 20:43:36 +0300

    Doneee

--- javscrepts.js
+++ javscrepts.js
@@ -1,15 +1,1 @@
-<script type="text/javascript" async="" defer="" src="//analytics.expandcart.com/piwik.js"></script><script type="application/ld+json">
-        {
-          "@context": "http://schema.org",
-          "@type": "Store",
-          "name": "Choco Mania ",
-          "description": "  Choco Mania ",
-          "url": "https://trychocomania.com/",
-          "image": "",
-          "logo": "",
-          "openingHours": "Sa-Fr 00:00-23:59",
-          "telephone": "05123456789",
-          "address": "https://trychocomania.com/",
-          "priceRange": "$$$"
-        }
-    </script>
+const counter = document.querySelector(".counterButton");
